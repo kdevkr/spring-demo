db891266cc1474f9bec9c8757c0da4455bffcdf1
Mambo <kdevkr@gmail.com>
2023-03-06 21:22:56 +0900

    feat: Distributed lock using shedlock

--- src/main/java/kr/kdev/demo/scheduler/ApplicationStartupNotifier.java
+++ src/main/java/kr/kdev/demo/scheduler/ApplicationStartupNotifier.java
@@ -1,6 +1,7 @@
 package kr.kdev.demo.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.time.Instant;
 @Component
 public class ApplicationStartupNotifier {
 
+    @SchedulerLock(name = "ApplicationStartupNotifier.info", lockAtLeastFor = "PT9M", lockAtMostFor = "PT9M59S")
     @Scheduled(cron = "0 0/10 * * * ?")
     public void info() {
         String appStartTime = System.getProperty("APP_START_TIME");
--- src/main/java/kr/kdev/demo/scheduler/SchedulingConfiguration.java
+++ src/main/java/kr/kdev/demo/scheduler/SchedulingConfiguration.java
@@ -1,11 +1,24 @@
 package kr.kdev.demo.scheduler;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT1M")
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty(prefix = "spring.task.scheduling", value = "enabled", havingValue = "true")
 public class SchedulingConfiguration {
+
+    @Bean
+    public LockProvider lockProvider(RedisConnectionFactory connectionFactory, Environment environment) {
+        String env = environment.getProperty("spring.task.scheduling.env", String.class, "default");
+        return new RedisLockProvider(connectionFactory, env);
+    }
 }
