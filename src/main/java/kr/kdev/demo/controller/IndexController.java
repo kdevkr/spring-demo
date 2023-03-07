aed02f456d13316aee257aaa04eda49a52e07320
Mambo <kdevkr@gmail.com>
2023-03-07 22:38:08 +0900

    feat: Add i18n from messages.xml

--- src/main/java/kr/kdev/demo/controller/IndexController.java
+++ src/main/java/kr/kdev/demo/controller/IndexController.java
@@ -1,13 +1,24 @@
 package kr.kdev.demo.controller;
 
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
+
 @RestController
 public class IndexController {
+
+    private final MessageSource messageSource;
+
+    public IndexController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/")
-    public ResponseEntity<String> index() {
-        return ResponseEntity.ok("OK");
+    public ResponseEntity<String> index(Locale locale) {
+        return ResponseEntity.ok(messageSource.getMessage("msg.hello", null, locale));
     }
 }
---
+++ src/main/java/kr/kdev/demo/i18n/MessageConfiguration.java
@@ -0,0 +1,42 @@
+package kr.kdev.demo.i18n;
+
+import org.springframework.boot.autoconfigure.context.MessageSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.StringUtils;
+
+import java.time.Duration;
+
+@Configuration
+public class MessageConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.messages")
+    public MessageSourceProperties messageSourceProperties() {
+        return new MessageSourceProperties();
+    }
+
+    @Bean
+    public MessageSource messageSource(MessageSourceProperties properties, ResourceLoader resourceLoader) {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        if (StringUtils.hasText(properties.getBasename())) {
+            messageSource.setBasenames(StringUtils
+                .commaDelimitedListToStringArray(StringUtils.trimAllWhitespace(properties.getBasename())));
+        }
+        if (properties.getEncoding() != null) {
+            messageSource.setDefaultEncoding(properties.getEncoding().name());
+        }
+        messageSource.setFallbackToSystemLocale(properties.isFallbackToSystemLocale());
+        Duration cacheDuration = properties.getCacheDuration();
+        if (cacheDuration != null) {
+            messageSource.setCacheMillis(cacheDuration.toMillis());
+        }
+        messageSource.setAlwaysUseMessageFormat(properties.isAlwaysUseMessageFormat());
+        messageSource.setUseCodeAsDefaultMessage(properties.isUseCodeAsDefaultMessage());
+        messageSource.setResourceLoader(resourceLoader);
+        return messageSource;
+    }
+}
